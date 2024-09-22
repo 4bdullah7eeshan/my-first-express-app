57068a84ef75124828fb3402f47dc644c91a2093
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-09-23 04:48:25 +0530

    feat: add a new get method for '/messages' route

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => res.send("hello, world"));
-app.post("/messages", (req, res) => res.send("This is where you can see any messages."));
+//app.post("/messages", (req, res) => res.send("This is where you can see any messages."));
+app.get("/messages", (req, res) => res.send("This is where you can see any messages."));
 
 const PORT = process.env.PORT || 3000;
 
