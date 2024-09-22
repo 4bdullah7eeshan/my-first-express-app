2cdc6425f359742cb03da0e8dab7516eca5e0446
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-09-23 04:56:40 +0530

    feat: try route parameters

--- app.js
+++ app.js
@@ -3,10 +3,19 @@ const app = express();
 
 app.get("/", (req, res) => res.send("hello, world"));
 //app.post("/messages", (req, res) => res.send("This is where you can see any messages."));
-app.get("/messages", (req, res) => res.send("This is where you can see any messages."));
+// app.get("/messages", (req, res) => res.send("This is where you can see any messages."));
 
-app.get("*", (req, res) => {
-    res.send("* is a great way to catch all otherwise unmatched paths, e.g. for custom 404 error handling.");
+// app.get("*", (req, res) => {
+//     res.send("* is a great way to catch all otherwise unmatched paths, e.g. for custom 404 error handling.");
+// });
+app.get("/:username/messages", (req, res) => {
+     console.log(req.params);
+     res.end();
+});
+
+app.get("/:username/messages/:messageId", (req, res) => {
+    console.log(req.params);
+    res.end();
 });
 
 const PORT = process.env.PORT || 3000;
