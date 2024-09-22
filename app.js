1255b43db3caed855b06f4130a4f8d6a7391030d
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-09-23 04:50:23 +0530

    refactor: place the '/messages' route before '*'

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const app = express();
 
 app.get("/", (req, res) => res.send("hello, world"));
 //app.post("/messages", (req, res) => res.send("This is where you can see any messages."));
+app.get("/messages", (req, res) => res.send("This is where you can see any messages."));
+
 app.get("*", (req, res) => {
     res.send("* is a great way to catch all otherwise unmatched paths, e.g. for custom 404 error handling.");
 });
-app.get("/messages", (req, res) => res.send("This is where you can see any messages."));
 
 const PORT = process.env.PORT || 3000;
 
