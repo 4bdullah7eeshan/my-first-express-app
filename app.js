ce6e4a14a61f25b3c6384c821f81f092c3603b0a
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-09-23 03:35:40 +0530

    refactor: allow port number to be taken from env

--- app.js
+++ app.js
@@ -3,5 +3,6 @@ const app = express();
 
 app.get("/", (req, res) => res.send("hello, world"));
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => console.log(`My first Express app, listening on port: ${PORT}!`));
