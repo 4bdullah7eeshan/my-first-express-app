3b0e930e802c6d48fe45f13a57f711159827c237
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-09-23 05:13:33 +0530

    feat: add routers

--- app.js
+++ app.js
@@ -1,29 +1,12 @@
 const express = require("express");
+const authorsRouter = require("./routes/authorsRouter");
+
 const app = express();
 
-app.get("/", (req, res) => res.send("hello, world"));
-//app.post("/messages", (req, res) => res.send("This is where you can see any messages."));
-// app.get("/messages", (req, res) => res.send("This is where you can see any messages."));
+app.get("/", (req, res) => res.send("Hello, world!"));
+app.use("/authors", authorsRouter);
 
-// app.get("*", (req, res) => {
-//     res.send("* is a great way to catch all otherwise unmatched paths, e.g. for custom 404 error handling.");
-// });
-// app.get("/:username/messages", (req, res) => {
-//      console.log(req.params);
-//      res.end();
-// });
-
-// app.get("/:username/messages/:messageId", (req, res) => {
-//     console.log(req.params);
-//     res.end();
-// });
-
-app.get("/:username/messages", (req, res) => {
-    console.log("Params:", req.params);
-    console.log("Query:", req.query);
-    res.end();
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`My first Express app - listening on port ${PORT}!`);
 });
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => console.log(`My first Express app, listening on port: ${PORT}!`));
